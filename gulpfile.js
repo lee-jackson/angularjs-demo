f9dcaa615e06e87ca0a078a1c89a936608a81531
Lee Jackson <dysfunctional.apps@gmail.com>
2017-10-09 18:40:28 +0100

    Added sass linting (using default rules)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp')
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    sassLint = require('gulp-sass-lint');
 
-gulp.task('sass', function(){
+gulp.task('sassLint', function() {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
+
+gulp.task('sass', ['sassLint'], function(){
   return gulp.src('sass/styles.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
