7eb3bcea24832d4e67d308571e54fb2308cf4d09
Lee Jackson <dysfunctional.apps@gmail.com>
2017-10-15 11:12:04 +0100

    Created form in template

--- scripts/modules/contact/index.js
+++ scripts/modules/contact/index.js
@@ -1,1 +1,2 @@
+require('./controllers');
 require('./routes');
--- scripts/modules/contact/routes/route.js
+++ scripts/modules/contact/routes/route.js
@@ -2,7 +2,10 @@ var route = function($stateProvider) {
   $stateProvider
     .state('contact', {
       url: '/contact',
-      template: require('../partials/contact.html')
+      template: require('../partials/contact.html'),
+      controller: 'ContactController',
+      controllerAs: 'ctrl',
+      bindToController: true
     });
 };
 
